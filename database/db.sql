42ba3e7e8653c1bd2626bdfeafd00749a92afbec
rioanggoro <rioanggoro37@gmail.com>
2025-03-08 15:42:36 +0700

    fix:change icon navbar

---
+++ database/db.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 08, 2025 at 09:07 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecommerce_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(191) NOT NULL,
+  `email` varchar(191) DEFAULT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(191) DEFAULT NULL,
+  `photo` varchar(191) DEFAULT NULL,
+  `role` enum('admin','user') NOT NULL DEFAULT 'user',
+  `provider` varchar(191) DEFAULT NULL,
+  `provider_id` varchar(191) DEFAULT NULL,
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `photo`, `role`, `provider`, `provider_id`, `status`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Admin', 'admin@gmail.com', NULL, '$2y$10$Yci9BOM.mYM6dta.eWrBFuePfUdcN3VeFDoVqrAsspQkPx3qnV5nW', '/storage/photos/1/users/user1.jpg', 'admin', NULL, NULL, 'active', 'OfLVCS5Ob0EcuNHEySf2vUwRUIK5X4682lssI0l28WMpmLKsgKFvDL8TKRJB', NULL, '2020-08-15 06:47:13'),
+(2, 'User', 'user@gmail.com', NULL, '$2y$10$10jB2lupSfvAUfocjguzSeN95LkwgZJUM7aQBdb2Op7XzJ.BhNoHq', '/storage/photos/1/users/user2.jpg', 'user', NULL, NULL, 'active', NULL, NULL, '2020-08-15 07:30:07'),
+(3, 'Prajwal Rai', 'prajwal.iar@gmail.com', NULL, '$2y$10$15ZVMgH040v4Ukf9KSAFiucPJcfDwmeRKCaguVJBXplTs93m48F1G', '/storage/photos/1/users/user3.jpg', 'user', NULL, NULL, 'active', NULL, '2020-08-11 04:20:58', '2020-08-15 07:56:58'),
+(4, 'Cynthia Beier', 'ernestina.wehner@example.net', '2020-08-14 21:18:52', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, 'user', NULL, NULL, 'active', 'fzmQDfEoaP', '2020-08-14 21:18:52', '2020-08-14 21:18:52'),
+(5, 'Prof. Maybell Zulauf', 'wolf.harvey@example.org', '2020-08-14 21:18:52', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, 'user', NULL, NULL, 'active', 'B8cYq4huyT', '2020-08-14 21:18:54', '2020-08-14 21:18:54'),
+(6, 'Diego Lind II', 'schroeder.emile@example.net', '2020-08-14 21:18:52', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', NULL, 'user', NULL, NULL, 'active', 'xLUaF26dE1', '2020-08-14 21:18:54', '2020-08-14 21:18:54'),
+(30, 'rioanggoro', 'rio@gmail.com', NULL, '$2y$10$HL2BnBA4eUpbqLzhMbUu/.46LTz8NG8yOiyzvhDGuEPs8ufj9IK5a', NULL, 'user', NULL, NULL, 'active', NULL, '2025-03-07 22:31:13', '2025-03-07 22:31:13');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -1,72 +1,61 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
     <div class="container">
-      <a class="navbar-brand fw-bold" href="{{route('home')}}">Prolific</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav me-auto">
-          <li class="nav-item"><a class="nav-link" href="{{route('product-grids')}}">Toko</a></li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route('about-us')}}">Tentang Kami</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav">
-          <li class="nav-item position-relative">
-            <a class="nav-link" href="{{route('wishlist')}}">
-              <i class="fa-solid fa-heart"></i>
-              <span
-                id="wishlist-count"
-                class="position-absolute top-25 start-70 translate-middle badge rounded-pill bg-danger opacity-75"
-              >
-                {{Helper::wishlistCount()}}
-              </span>
-            </a>
-          </li>
-          <li class="nav-item position-relative">
-            <a class="nav-link" href="{{route('cart')}}">
-              <i class="fa-solid fa-shopping-cart"></i>
-              <span
-                id="cart-count"
-                class="position-absolute top-25 start-70 translate-middle badge rounded-pill bg-danger opacity-75"
-              >
-                {{Helper::cartCount()}}
-              </span>
-            </a>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="userDropdown"
-              role="button"
-              data-bs-toggle="dropdown"
-            >
-              <i class="fa-solid fa-user"></i>
-            </a>
-            <ul
-              class="dropdown-menu dropdown-menu-end"
-              aria-labelledby="userDropdown"
-            >
-            @auth
-                @if(Auth::user()->role=='admin')
-                    <li><a class="dropdown-item" href="{{route('admin')}}"  target="_blank"><i class="fa-solid fa-user"></i> Dashboard</a></li>
-                @else
-                    <li><i class="ti-user"></i><a class="dropdown-item" href="{{route('user')}}"><i class="fa-solid fa-user"></i> Dashboard</a></li>
-                @endif
-                    <li><a class="dropdown-item" href="{{route('user.logout')}}"><i class="fa-solid fa-power-off"></i> Logout</a></li>
-                @else
-                    <li><a class="dropdown-item" href="{{route('login.form')}}">Masuk</a></li>
-                    <li><a class="dropdown-item" href="{{route('register.form')}}">Register</a></li>
-            @endauth
+        <a class="navbar-brand fw-bold" href="{{ route('home') }}">
+            <img src="{{ asset('backend/img/logo_primary.png') }}" alt="Logo Prolific" style="height: 30px; width: auto;">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item"><a class="nav-link" href="{{ route('product-grids') }}">Toko</a></li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('about-us') }}">Tentang Kami</a>
+                </li>
             </ul>
-          </li>
-        </ul>
-      </div>
+            <ul class="navbar-nav">
+                <li class="nav-item position-relative">
+                    <a class="nav-link" href="{{ route('wishlist') }}">
+                        <i class="fa-solid fa-heart"></i>
+                        <span id="wishlist-count"
+                            class="position-absolute top-25 start-70 translate-middle badge rounded-pill bg-danger opacity-75">
+                            {{ Helper::wishlistCount() }}
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item position-relative">
+                    <a class="nav-link" href="{{ route('cart') }}">
+                        <i class="fa-solid fa-shopping-cart"></i>
+                        <span id="cart-count"
+                            class="position-absolute top-25 start-70 translate-middle badge rounded-pill bg-danger opacity-75">
+                            {{ Helper::cartCount() }}
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                        data-bs-toggle="dropdown">
+                        <i class="fa-solid fa-user"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                        @auth
+                            @if (Auth::user()->role == 'admin')
+                                <li><a class="dropdown-item" href="{{ route('admin') }}" target="_blank"><i
+                                            class="fa-solid fa-user"></i> Dashboard</a></li>
+                            @else
+                                <li><i class="ti-user"></i><a class="dropdown-item" href="{{ route('user') }}"><i
+                                            class="fa-solid fa-user"></i> Dashboard</a></li>
+                            @endif
+                            <li><a class="dropdown-item" href="{{ route('user.logout') }}"><i
+                                        class="fa-solid fa-power-off"></i> Logout</a></li>
+                        @else
+                            <li><a class="dropdown-item" href="{{ route('login.form') }}">Masuk</a></li>
+                            <li><a class="dropdown-item" href="{{ route('register.form') }}">Register</a></li>
+                        @endauth
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
-  </nav>
+</nav>
